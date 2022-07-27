8d4e273fe6042eb8a0cabe4401f3a60b76a330a3
max <max.lillie95@gmail.com>
2022-07-27 12:58:30 -0500

    added calls test file, changed products_controller to show banana and added the /banana route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def get_info_banana
+    product = Product.find_by title: "Banana"
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #2 routes, shows 1 product, shows all products
+
+  get "/banana", controller: "products", action: "get_info_banana"
 
   # Defines the root path route ("/")
   # root "articles#index"
