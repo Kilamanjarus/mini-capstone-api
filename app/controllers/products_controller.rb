a3839c0b5d98a575228099ea516f8ed0cadc7019
max <max.lillie95@gmail.com>
2022-07-28 12:41:02 -0500

    Added hard coded create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      title: "Carrot",
+      description: "A orange vegetable",
+      price: 1,
+      image_url: "google.com",
+    )
+    product.save
+    render json: { message: "Good job, saved!" }
+  end
 end
