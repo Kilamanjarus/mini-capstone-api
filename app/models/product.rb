ddae1eb81951e75f621c64938140917dd98b97bd
max <max.lillie95@gmail.com>
2022-08-01 13:58:01 -0500

    added friendly_created_at and is_discounted? methods to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    product_price = price
+    if product_price < 10
+      return true
+    else
+      return false
+    end
+  end
 end
