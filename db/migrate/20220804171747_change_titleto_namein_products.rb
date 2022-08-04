e7f32c93c7485f5d5aa702f391bb7c7e3ab0c013
max <max.lillie95@gmail.com>
2022-08-04 12:21:41 -0500

    Added 3 migrations, creating suppliers table, adding suppliers id to products,a nd renaming title in products to name

---
+++ db/migrate/20220804171747_change_titleto_namein_products.rb
@@ -0,0 +1,5 @@
+class ChangeTitletoNameinProducts < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :products, :title, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_185957) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_171747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.text "title"
+    t.text "name"
     t.string "description"
     t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventoryamount"
+    t.string "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
