a2eaa96ae2e6d6d7f34997803b7bae5a840c6adf
max <max.lillie95@gmail.com>
2022-08-04 14:06:30 -0500

    removed image_url from Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_190120) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_190512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_190120) do
     t.text "name"
     t.string "description"
     t.decimal "price", precision: 9, scale: 2
-    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventoryamount"
